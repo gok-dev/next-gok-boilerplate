53d0e42128cc9bc65b8cb28c2a4f9fbf96350ba3
Julio Augusto <julioams@icloud.com>
2020-11-13 19:46:52 -0300

    refactor storybook for controls

--- src/components/main/stories.tsx
+++ src/components/main/stories.tsx
@@ -1,19 +1,13 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS and Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'React Avançado',
+  description: 'TypeScript, ReactJS, NextJS and Styled Components'
+}
